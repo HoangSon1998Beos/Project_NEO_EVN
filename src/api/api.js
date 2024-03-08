9552991d49b034b632dbfe4ef7d69d0958ef2222
nghiadh <daohuunghia280996@gmail.com>
2024-03-08 15:59:59 +0700

    feat: get token after login

--- src/api/api.js
+++ src/api/api.js
@@ -7,8 +7,7 @@ import DashbroadRepository from "./darhboard/DashbroadRepository.js";
 axios.interceptors.request.use(
     function (config) {
         const baseURL = import.meta.env.VITE_API_BASE_URL
-        const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsIm5hbWUiOiJBZG1pbiIsInR5cGUiOiJBRE1JTiIsImlkIjoxMTksImlhdCI6MTcwOTc5OTU1MCwiZXhwIjoxNzA5ODg1OTUwfQ.fZMewRkx4v8zSgdgFWeRGAad7R0VglxtrUGwyTfTH91Aix2XSCKCPUHiOzrmHo0CHJUgqDKHcf4EDl6qwso3og'
-        // const token = localStorage.getItem('token')
+        const token = localStorage.getItem('token')
         config.baseURL = baseURL
         config.headers.Authorization = `Bearer ${token}`
         return config
