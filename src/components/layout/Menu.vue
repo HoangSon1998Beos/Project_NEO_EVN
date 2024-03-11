11c72d9df50ab7c67959c9531c7c376ebc5a9cd1
HoangSon1998Beos <sonhdev97@gmail.com>
2024-03-11 15:26:37 +0700

    menu1

--- src/components/layout/Menu.vue
+++ src/components/layout/Menu.vue
@@ -104,9 +104,9 @@ export default {
   data: () => ({
     qtht: [
       { title: "Quản lý người dùng", to: "/person" },
-      { title: "Quản lý Menu", to: "" },
-      { title: "Quản lý khách hàng", to: "" },
-      { title: "Quản lý các phiên bản sao lưu", to: "" },
+      { title: "Quản lý Menu" },
+      { title: "Quản lý khách hàng" },
+      { title: "Quản lý các phiên bản sao lưu" },
     ],
     dtcb: [
       { title: "Quản lý Bot", path: "/bot" },
